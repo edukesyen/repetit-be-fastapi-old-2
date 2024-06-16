117df6a19b86b2263bc01090a6f61000a7193e84
Almaas Izdihar <almaasizdihar@gmail.com>
2024-06-16 14:29:42 +0700

    setup db environment variable

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+from app.core.config import settings  # env
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,11 +30,12 @@ target_metadata = SQLModel.metadata
 
 
 def get_url():
-    user = os.getenv("POSTGRES_USER", "postgres")
-    password = os.getenv("POSTGRES_PASSWORD", "")
-    server = os.getenv("POSTGRES_SERVER", "db")
-    port = os.getenv("POSTGRES_PORT", "5432")
-    db = os.getenv("POSTGRES_DB", "app")
+    user = settings.POSTGRES_USER
+    password = settings.POSTGRES_PASSWORD
+    server = settings.POSTGRES_SERVER
+    port = settings.POSTGRES_PORT
+    db = settings.POSTGRES_DB
+
     return f"postgresql+psycopg://{user}:{password}@{server}:{port}/{db}"
 
 
