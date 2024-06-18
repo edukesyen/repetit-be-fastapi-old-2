cf7b12471b256bc2a804215d56ff08c7cdbd2eed
Almaas Izdihar <almaasizdihar@gmail.com>
2024-06-18 14:32:16 +0700

    add experimental endpoint

---
+++ app/api/routes/labs.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+router = APIRouter()
+
+@router.get("/hello-world")
+def hello_world ():
+    res = {
+        "message": "Hello World"
+    }
+    return JSONResponse(content=jsonable_encoder(res))
